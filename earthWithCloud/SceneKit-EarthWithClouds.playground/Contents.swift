86f8cbad574b6f997090daadb5c90d23f45a19a6
Shakir Ali <shakirmithani@gmail.com>
2016-12-26 15:17:28 +0000

    Added sample for EarthWithClouds

---
+++ earthWithCloud/SceneKit-EarthWithClouds.playground/Contents.swift
@@ -0,0 +1,84 @@
+import UIKit
+import SceneKit
+import QuartzCore
+import PlaygroundSupport
+
+/*: SceneKit Sample- Earth with Clouds
+# Introduction
+ In this sample I have animated Earth globe with clouds using SceneKit.
+ **Note**
+This playground is based on the code and resources provided in the WWDC 2013 - SceneKit session sample code which is available for free to download. My aim is to make small samples out of that presentation.
+*/
+
+//Create SceneView
+var sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+var scene = SCNScene()
+sceneView.scene = scene
+sceneView.backgroundColor = UIColor.black
+
+// start a live preview of that view
+PlaygroundPage.current.liveView = sceneView
+
+// 3. default lighting
+sceneView.autoenablesDefaultLighting = true
+
+//camera
+var cameraNode = SCNNode()
+cameraNode.camera = SCNCamera()
+cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+scene.rootNode.addChildNode(cameraNode)
+
+//cloud node
+let cloudNode = SCNNode()
+cloudNode.geometry = SCNSphere(radius: 5.7)
+cloudNode.opacity = 0.9
+cloudNode.geometry?.firstMaterial?.transparent.contents = UIImage(named: "cloudsTransparency")
+cloudNode.geometry?.firstMaterial?.transparencyMode = .rgbZero
+cloudNode.geometry?.firstMaterial?.transparency = 1.0
+
+//earth node
+let earthNode = SCNNode()
+earthNode.geometry = SCNSphere(radius: 5)
+earthNode.opacity = 1.0
+earthNode.position = SCNVector3(0, 0, 0)
+let transform3D = CATransform3DMakeRotation(CGFloat(M_PI * 0.1), 1, 0, 0)
+earthNode.pivot = SCNMatrix4.makeSCNMatrix4(from: transform3D)
+
+//add cloud node
+earthNode.addChildNode(cloudNode)
+
+//diffuse
+earthNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "earthDiffuse.jpg")
+
+//shininess
+earthNode.geometry?.firstMaterial?.shininess = 0.1
+earthNode.geometry?.firstMaterial?.specular.contents = UIImage(named: "earthSpecular.jpg")
+
+//earth Bump
+earthNode.geometry?.firstMaterial?.normal.contents = UIImage(named: "earthBump")
+earthNode.geometry?.firstMaterial?.normal.intensity = 1.3
+
+//emissive
+earthNode.geometry?.firstMaterial?.emission.contents = UIImage(named:"earthEmissive.jpg")
+earthNode.geometry?.firstMaterial?.emission.intensity = 1.0
+
+//reflective
+earthNode.geometry?.firstMaterial?.reflective.contents = UIImage(named:"earthReflective.jpg")
+earthNode.geometry?.firstMaterial?.reflective.intensity = 0.1;
+
+//uncomment this line to see different effect of the material.
+//earthNode.geometry?.firstMaterial?.multiply.contents = UIColor(red: 1.0, green: 204/255.0, blue: 102/255.0, alpha: 1)
+
+//earth node animation
+var rotation = CABasicAnimation(keyPath: "rotation")
+rotation.duration = 40.0
+rotation.repeatCount = FLT_MAX
+rotation.toValue = NSValue(scnVector4: SCNVector4(x:0, y: 1, z: 0, w: Float(M_PI * 2)))
+earthNode.addAnimation(rotation, forKey: nil)
+
+rotation.duration = 100.0
+cloudNode.addAnimation(rotation, forKey: nil)
+
+
+//add node to scene
+scene.rootNode.addChildNode(earthNode)
---
+++ earthWithCloud/SceneKit-EarthWithClouds.playground/Sources/SCNMatrix4Extension.swift
@@ -0,0 +1,8 @@
+import SceneKit
+
+extension SCNMatrix4 {
+    public static func makeSCNMatrix4(from transform3D: CATransform3D) -> SCNMatrix4 {
+        let matrix = SCNMatrix4(m11: Float(transform3D.m11), m12: Float(transform3D.m12), m13: Float(transform3D.m13), m14: Float(transform3D.m14), m21: Float(transform3D.m21), m22: Float(transform3D.m22), m23: Float(transform3D.m23), m24: Float(transform3D.m24), m31: Float(transform3D.m31), m32: Float(transform3D.m32), m33: Float(transform3D.m33), m34: Float(transform3D.m34), m41: Float(transform3D.m41), m42: Float(transform3D.m42), m43: Float(transform3D.m43), m44: Float(transform3D.m44))
+        return matrix
+    }
+}
